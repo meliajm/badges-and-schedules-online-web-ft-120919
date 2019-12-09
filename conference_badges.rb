d5b40937c3a7f07fa4dffed6726bf99ffb6a3746
melia miller <meliajm@gmail.com>
2019-12-09 23:39:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  array_badge_messages = []
+  attendees.each do |name|
+    array_badge_messages.push(badge_maker(name))
+  end
+  array_badge_messages
+  
+end
+
+def assign_rooms(attendees)
+  room_assignment_greeting_array = []
+  attendees.each_with_index do |name, index|
+    room_assignment_greeting_array.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  end 
+  room_assignment_greeting_array
+  # binding.pry
+end
+
+def printer(attendees)
+  puts assign_rooms(attendees)
+  
+  # attendees.each do |name|
+  #   puts badge_maker(name)
+  # end 
+end 
+
+
+
